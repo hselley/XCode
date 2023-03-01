1e045c480845426ac628a33ab3303e071d96ee49
Héctor Selley <hector.selley@gmail.com>
2023-02-28 21:05:35 -0600

    Cambios menores

--- FoodPin Chapter 7.5/FoodPin/RestaurantListView.swift
+++ FoodPin Chapter 7.5/FoodPin/RestaurantListView.swift
@@ -9,24 +9,32 @@ import SwiftUI
 
 struct RestaurantListView: View {
     
-    var restaurantNames =
-        ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster",
-         "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery",
-         "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
-         "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves",
-         "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak",
-         "CASK Pub and Kitchen"]
+    var restaurantNames = [
+        "Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster",
+        "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery",
+        "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
+        "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves",
+        "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak",
+        "CASK Pub and Kitchen"]
     
-    var restaurantImages =
-        ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster",
-         "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino",
-         "upstate", "traif", "graham", "waffleandwolf", "fiveleaves",
+    var restaurantImages = [
+        "cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster",
+        "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino",
+        "upstate", "traif", "graham", "waffleandwolf", "fiveleaves",
          "cafelore", "confessional", "barrafina", "donostia", "royaloak",
          "cask"]
     
-    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantLocations = [
+        "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong",
+        "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York",
+        "New York", "New York", "New York", "New York", "New York", "New York",
+        "London", "London", "London", "London"]
         
-    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    var restaurantTypes = [
+        "Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink",
+        "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood",
+        "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea",
+        "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
     var body: some View {
         List {
